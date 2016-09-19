a5f7931f864927cd207514bbec7855d05df7e35a
valerieshoskes <indi.vs@gmail.com>
2016-09-19 16:00:56 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoskesRainAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<bool> boolList = new List<bool>() { true, false, false, true, false };
+            foreach (bool item in boolList)
+            {
+                if (item)
+                {
+                    Console.WriteLine("Better bring an umbrella");
+                }
+                else
+                {
+                    Console.WriteLine("No rain today, enjoy the sun!");
+                }
+            }
+        }
+    }
+}
